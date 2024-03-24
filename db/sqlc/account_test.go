8846526cdacbcaf9ecc7a38b1d5c84019844fc02
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-03-24 16:10:13 +0700

    handle DB errors in Golang correctly

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,8 +11,10 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(), // random generated?
+		Owner:    user.Username, // random generated?
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
